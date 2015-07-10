bbcf754bec1a3604655f14ea25072c93324b5b4a
Rune Tvilum <rune@addin.dk>
2015-07-10 11:49:36 +0200

    port

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,7 +42,7 @@ angular.module('starter.controllers', [])
         });
     };*/
     $scope.signIn = function (user) {
-        $http.post('http://bykongen.addin.dk/_chat217/signin', user).then(function (res) {
+        $http.post('http://bykongen.addin.dk/_chat217', user).then(function (res) {
             Auth.$authWithCustomToken(res.data).then(function (authData) {
                 console.log("Authentication succes:", authData.uid);
             }).catch(function (error) {
